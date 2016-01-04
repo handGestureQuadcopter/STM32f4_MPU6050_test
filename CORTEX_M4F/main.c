8a0c781d075199910ee832ad8eed7403972d92d4
kaizsv <kai.zsv@gmail.com>
2016-01-04 13:47:24 +0800

    mpu6050 test program on STM32f429

---
+++ CORTEX_M4F/main.c
@@ -0,0 +1,367 @@
+//#include <stdio.h>
+
+#include "stm32f4xx.h"
+#include "stm32f4xx_i2c.h"
+#include "stm32f429i_discovery.h"
+
+#include "mpu6050.h"
+
+#define MUP6050_ADDRESS 0x68
+
+unsigned int strlen(const char *str)
+{
+	unsigned int i = 0;
+	for(i=0; str[i]!='\0'; i++);
+	return i;
+}
+
+void reverse(char *str)
+{
+	unsigned int i = 0;
+	unsigned int length = strlen(str) - 1;
+	char c;
+
+	for (i = 0; i < length; i++, length--) {
+		c = str[i];
+		str[i] = str[length];
+		str[length] = c;
+	}
+}
+
+void itoa(uint16_t n, char *str)
+{
+	int i = 0, sign;
+	if ((sign = n) < 0)
+		n = -n;
+	do {
+		str[i++] = n % 10 + '0';
+	} while((n /= 10) > 0);
+	if (sign < 0)
+		str[i++] = '-';
+	str[i] = '\0';
+	reverse(str);
+}
+
+void USART1_puts(char* s)
+{
+    while(*s) {
+        while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+        USART_SendData(USART1, *s);
+        s++;
+    }
+}
+
+void I2C_Config(void)
+{
+
+
+    /* 
+    *         SCL = PB6
+    *         SDA = PB7
+    */
+
+
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  
+  /* Enable IOE_I2C and IOE_I2C_GPIO_PORT & Alternate Function clocks */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB | RCC_AHB1Periph_GPIOB , ENABLE);
+
+  
+  /* Connect PXx to I2C_SCL*/
+  GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_I2C1);
+  /* Connect PXx to I2C_SDA*/
+  GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_I2C1); 
+    
+  /* SCL and SDA pins configuration */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_25MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_UP;
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+  NVIC_InitTypeDef NVIC_InitStructure;
+
+  /* Configure the I2C event priority */
+  NVIC_InitStructure.NVIC_IRQChannel                   = I2C1_ER_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority        = 0;
+  NVIC_InitStructure.NVIC_IRQChannelCmd                = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+  
+ 
+
+        /* Enable the I2C peripheral */
+  I2C_DeInit( I2C1);
+        
+  I2C_InitTypeDef I2C_InitStructure;
+
+  I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
+  I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_2;
+  I2C_InitStructure.I2C_OwnAddress1 = 0x00;
+  I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;
+//  I2C_InitStructure.I2C_Ack = I2C_Ack_Disable;
+  I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+  I2C_InitStructure.I2C_ClockSpeed = 400000;
+//  I2C_InitStructure.I2C_ClockSpeed = I2C_SPEED;
+  
+  /* Initialize the I2C peripheral */
+  I2C_Init( I2C1 , &I2C_InitStructure);
+
+ 
+       
+  I2C_ITConfig(I2C1,I2C_IT_ERR , ENABLE);
+  I2C_Cmd( I2C1 , ENABLE);
+  
+}
+
+
+void I2C1_ER_IRQHandler(void)
+{
+  /* ACK failure */
+  
+  if (I2C_GetITStatus(I2C1, I2C_IT_AF)) 
+  {
+    USART1_puts("I2C_IT_AF");
+    I2C_ClearITPendingBit(I2C1, I2C_IT_AF);
+  }
+  else if(I2C_GetITStatus(I2C1, I2C_IT_BERR))
+  {
+    USART1_puts("I2C_IT_BERR");
+  }
+  else if(I2C_GetITStatus(I2C1, I2C_IT_ARLO))
+  {
+    USART1_puts("I2C_IT_ARLO");
+  }
+    else if(I2C_GetITStatus(I2C1, I2C_IT_OVR))
+  {
+    USART1_puts("I2C_IT_OVR");
+  }
+    else if(I2C_GetITStatus(I2C1, I2C_IT_PECERR))
+  {
+    USART1_puts("I2C_IT_PECERR");
+  }
+    else if(I2C_GetITStatus(I2C1, I2C_IT_TIMEOUT))
+  {
+    USART1_puts("I2C_IT_TIMEOUT");
+  }
+  else if(I2C_GetITStatus(I2C1, I2C_IT_SMBALERT))
+  {
+    USART1_puts("I2C_IT_SMBALERT");
+  }
+    
+}
+
+
+uint8_t I2C_start(I2C_TypeDef* I2Cx, uint8_t address, uint8_t direction)
+ {
+	// wait until I2C1 is not busy anymore
+	while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_BUSY));
+
+	// Send I2C1 START condition
+	I2C_GenerateSTART(I2Cx, ENABLE);
+
+	// wait for I2C1 EV5 --> Slave has acknowledged start condition
+	while (!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_MODE_SELECT));
+
+	// Send slave Address for write
+	I2C_Send7bitAddress(I2Cx, address, direction);
+
+	/* wait for I2C1 EV6, check if
+	 * either Slave has acknowledged Master transmitter or
+	 * Master receiver mode, depending on the transmission
+	 * direction
+	 */
+	if (direction == I2C_Direction_Transmitter) {
+		while (!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+	} else if (direction == I2C_Direction_Receiver) {
+		while (!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
+	}
+
+  return  0;
+}
+
+/* This function transmits one byte to the slave device
+* Parameters:
+* I2Cx --> the I2C peripheral e.g. I2C1
+* data --> the data byte to be transmitted
+*/
+void I2C_write(I2C_TypeDef* I2Cx, uint8_t data)
+{
+    I2C_SendData(I2Cx, data);
+    // wait for I2C1 EV8_2 --> byte has been transmitted
+    while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+}
+
+/* This funtion issues a stop condition and therefore
+* releases the bus
+*/
+void I2C_stop(I2C_TypeDef* I2Cx){
+    // Send I2C1 STOP Condition
+    I2C_GenerateSTOP(I2Cx, ENABLE);
+}
+
+/* This function reads one byte from the slave device
+* and acknowledges the byte (requests another byte)
+*/
+uint8_t I2C_read_ack(I2C_TypeDef* I2Cx){
+    uint8_t data;
+    // enable acknowledge of recieved data
+    I2C_AcknowledgeConfig(I2Cx, ENABLE);
+    // wait until one byte has been received
+    while( !I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_RECEIVED) );
+    // read data from I2C data register and return data byte
+    data = I2C_ReceiveData(I2Cx);
+    return data;
+}
+
+/* This function reads one byte from the slave device
+* and doesn't acknowledge the recieved data
+*/
+uint8_t I2C_read_nack(I2C_TypeDef* I2Cx){
+    uint8_t data;
+    // disabe acknowledge of received data
+    // nack also generates stop condition after last byte received
+    // see reference manual for more info
+    I2C_AcknowledgeConfig(I2Cx, DISABLE);
+    I2C_GenerateSTOP(I2Cx, ENABLE);
+    // wait until one byte has been received
+    while( !I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_RECEIVED) );
+    // read data from I2C data register and return data byte
+    data = I2C_ReceiveData(I2Cx);
+    return data;
+}
+
+void RCC_Configuration(void)
+{
+      /* --------------------------- System Clocks Configuration -----------------*/
+      /* USART1 clock enable */
+      RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+      /* GPIOA clock enable */
+      RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+}
+
+
+void GPIO_Configuration(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    /*-------------------------- GPIO Configuration ----------------------------*/
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    /* Connect USART pins to AF */
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);   // USART1_TX
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);  // USART1_RX
+}
+ 
+
+ void USART1_Configuration(void)
+{
+    USART_InitTypeDef USART_InitStructure;
+
+    /* USARTx configuration ------------------------------------------------------*/
+    /* USARTx configured as follow:
+     *  - BaudRate = 9600 baud
+     *  - Word Length = 8 Bits
+     *  - One Stop Bit
+     *  - No parity
+     *  - Hardware flow control disabled (RTS and CTS signals)
+     *  - Receive and transmit enabled
+     */
+    USART_InitStructure.USART_BaudRate = 115200;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART_Init(USART1, &USART_InitStructure);
+    USART_Cmd(USART1, ENABLE);
+}
+
+
+
+int main(){
+
+	TM_MPU6050_t MPU6050_Data0;
+	uint8_t sensor1 = 0;
+	char str[120];
+
+	RCC_Configuration();
+  	GPIO_Configuration();
+  	USART1_Configuration();	
+
+//	I2C_Config();
+
+	/* Initialize MPU6050 sensor 0, address = 0xD0, AD0 pin on sensor is low */
+	if (MPU6050_Init(&MPU6050_Data0, TM_MPU6050_Accelerometer_8G, TM_MPU6050_Gyroscope_250s)
+			== TM_MPU6050_Result_Ok) {
+		/* Display message to user */
+		USART1_puts("MPU6050 sensor 0 is ready to use!\n");
+
+		/* Sensor 1 OK */
+		sensor1 = 1;
+	}
+	
+	uint8_t received_data[2];
+	char uart_out[32];
+    	
+//	STM_EVAL_PBInit( BUTTON_USER, BUTTON_MODE_GPIO );
+
+	while(1){
+
+		/* If sensor 1 is connected */
+		if (sensor1) {
+			/* Read all data from sensor 1 */
+			MPU6050_ReadAll(&MPU6050_Data0);
+
+//			/* Format data */
+//			sprintf(str,
+//					"1. Accelerometer\n- X:%d\n- Y:%d\n- Z:%d\nGyroscope\n- X:%d\n- Y:%d\n- Z:%d\nTemperature\n- %3.4f\n\n\n",
+//					MPU6050_Data0.Accelerometer_X,
+//					MPU6050_Data0.Accelerometer_Y,
+//					MPU6050_Data0.Accelerometer_Z, MPU6050_Data0.Gyroscope_X,
+//					MPU6050_Data0.Gyroscope_Y, MPU6050_Data0.Gyroscope_Z,
+//					MPU6050_Data0.Temperature);
+//
+
+			USART1_puts("\nNext: ");
+			USART1_puts(" AX: ");
+			itoa(MPU6050_Data0.Accelerometer_X, uart_out);
+			USART1_puts(uart_out);
+			USART1_puts(" AY: ");
+			itoa(MPU6050_Data0.Accelerometer_Y, uart_out);
+			USART1_puts(uart_out);
+			USART1_puts(" AZ: ");
+			itoa(MPU6050_Data0.Accelerometer_Z, uart_out);
+			USART1_puts(uart_out);
+		}
+
+//			I2C_start(I2C1, MUP6050_ADDRESS, I2C_Direction_Transmitter);
+//			I2C_write(I2C1, 0x41);
+//			I2C_stop(I2C1);
+//
+//			// start a transmission in Master receiver mode
+//			I2C_start(I2C1, MUP6050_ADDRESS, I2C_Direction_Receiver);
+//
+//			received_data[0] = I2C_read_ack(I2C1); // read one byte and request another byte
+//			received_data[1] = I2C_read_nack(I2C1); // read one byte and don't request another byte, stop transmission
+//
+//			itoa(received_data[0], uart_out);
+//			USART1_puts(uart_out);
+//			itoa(received_data[1], uart_out);
+//			USART1_puts(uart_out);
+		for (int i = 0; i < 100000; i++);
+	}
+
+//	return 0;
+}
---
+++ CORTEX_M4F/mpu6050.c
@@ -0,0 +1,373 @@
+#include "mpu6050.h"
+
+static uint32_t MPU6050_I2C_Timeout;
+
+TM_MPU6050_Result_t MPU6050_Init(TM_MPU6050_t* DataStruct, TM_MPU6050_Accelerometer_t AccelerometerSensitivity, TM_MPU6050_Gyroscope_t GyroscopeSensitivity) {
+
+	uint8_t temp;
+	
+	MPU6050_I2C_Init();
+
+	/* Format I2C address */
+	DataStruct->Address = MPU6050_I2C_ADDR;
+	
+//	/* Initialize I2C */
+//	TM_I2C_Init(MPU6050_I2C, MPU6050_I2C_PINSPACK, MPU6050_I2C_CLOCK);
+	
+	/* Check if device is connected */
+	if (!MPU6050_I2C_IsDeviceConnected(MPU6050_I2C, DataStruct->Address)) {
+		/* Return error */
+		return TM_MPU6050_Result_DeviceNotConnected;
+	}
+	
+	/* Check who I am */
+	if (I2C_Read(MPU6050_I2C, DataStruct->Address, MPU6050_WHO_AM_I) != MPU6050_I_AM) {
+		/* Return error */
+		return TM_MPU6050_Result_DeviceInvalid;
+	}
+	
+	/* Wakeup MPU6050 */
+	I2C_Write(MPU6050_I2C, DataStruct->Address, MPU6050_PWR_MGMT_1, 0x00);
+	
+	/* Config accelerometer */
+	temp = I2C_Read(MPU6050_I2C, DataStruct->Address, MPU6050_ACCEL_CONFIG);
+	temp = (temp & 0xE7) | (uint8_t)AccelerometerSensitivity << 3;
+	I2C_Write(MPU6050_I2C, DataStruct->Address, MPU6050_ACCEL_CONFIG, temp);
+	
+	/* Config gyroscope */
+	temp = I2C_Read(MPU6050_I2C, DataStruct->Address, MPU6050_GYRO_CONFIG);
+	temp = (temp & 0xE7) | (uint8_t)GyroscopeSensitivity << 3;
+	I2C_Write(MPU6050_I2C, DataStruct->Address, MPU6050_GYRO_CONFIG, temp);
+	
+	/* Set sensitivities for multiplying gyro and accelerometer data */
+	switch (AccelerometerSensitivity) {
+		case TM_MPU6050_Accelerometer_2G:
+			DataStruct->Acce_Mult = (float)1 / MPU6050_ACCE_SENS_2; 
+			break;
+		case TM_MPU6050_Accelerometer_4G:
+			DataStruct->Acce_Mult = (float)1 / MPU6050_ACCE_SENS_4; 
+			break;
+		case TM_MPU6050_Accelerometer_8G:
+			DataStruct->Acce_Mult = (float)1 / MPU6050_ACCE_SENS_8; 
+			break;
+		case TM_MPU6050_Accelerometer_16G:
+			DataStruct->Acce_Mult = (float)1 / MPU6050_ACCE_SENS_16; 
+		default:
+			break;
+	}
+	
+	switch (GyroscopeSensitivity) {
+		case TM_MPU6050_Gyroscope_250s:
+			DataStruct->Gyro_Mult = (float)1 / MPU6050_GYRO_SENS_250; 
+			break;
+		case TM_MPU6050_Gyroscope_500s:
+			DataStruct->Gyro_Mult = (float)1 / MPU6050_GYRO_SENS_500; 
+			break;
+		case TM_MPU6050_Gyroscope_1000s:
+			DataStruct->Gyro_Mult = (float)1 / MPU6050_GYRO_SENS_1000; 
+			break;
+		case TM_MPU6050_Gyroscope_2000s:
+			DataStruct->Gyro_Mult = (float)1 / MPU6050_GYRO_SENS_2000; 
+		default:
+			break;
+	}
+	
+	/* Return OK */
+	return TM_MPU6050_Result_Ok;
+}
+
+void MPU6050_I2C_Init() {
+	/*
+	 *         SCL = PB6
+	 *         SDA = PB7
+	 */
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	/* Enable IOE_I2C and IOE_I2C_GPIO_PORT & Alternate Function clocks */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB | RCC_AHB1Periph_GPIOB, ENABLE);
+
+	/* Connect PXx to I2C_SCL*/
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_I2C1);
+	/* Connect PXx to I2C_SDA*/
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_I2C1);
+
+	/* SCL and SDA pins configuration */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_25MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	/* Configure the I2C event priority */
+	NVIC_InitStructure.NVIC_IRQChannel = I2C1_ER_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+	/* Enable the I2C peripheral */
+	I2C_DeInit(MPU6050_I2C);
+
+	I2C_InitTypeDef I2C_InitStructure;
+
+	I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
+	I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_2;
+	I2C_InitStructure.I2C_OwnAddress1 = 0x00;
+	I2C_InitStructure.I2C_Ack = I2C_Ack_Disable;
+	//  I2C_InitStructure.I2C_Ack = I2C_Ack_Disable;
+	I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+	I2C_InitStructure.I2C_ClockSpeed = MPU6050_I2C_CLOCK;
+	//  I2C_InitStructure.I2C_ClockSpeed = I2C_SPEED;
+
+	/* Initialize the I2C peripheral */
+	I2C_Init(MPU6050_I2C, &I2C_InitStructure);
+
+	I2C_ITConfig(MPU6050_I2C, I2C_IT_ERR, ENABLE);
+	I2C_Cmd(MPU6050_I2C, ENABLE);
+}
+
+TM_MPU6050_Result_t MPU6050_ReadAccelerometer(TM_MPU6050_t* DataStruct) {
+	uint8_t data[6];
+	
+	/* Read accelerometer data */
+	I2C_ReadMulti(MPU6050_I2C, DataStruct->Address, MPU6050_ACCEL_XOUT_H, data, 6);
+	
+	/* Format */
+	DataStruct->Accelerometer_X = (int16_t)(data[0] << 8 | data[1]);	
+	DataStruct->Accelerometer_Y = (int16_t)(data[2] << 8 | data[3]);
+	DataStruct->Accelerometer_Z = (int16_t)(data[4] << 8 | data[5]);
+	
+	/* Return OK */
+	return TM_MPU6050_Result_Ok;
+}
+
+TM_MPU6050_Result_t MPU6050_ReadGyroscope(TM_MPU6050_t* DataStruct) {
+	uint8_t data[6];
+	
+	/* Read gyroscope data */
+	I2C_ReadMulti(MPU6050_I2C, DataStruct->Address, MPU6050_GYRO_XOUT_H, data, 6);
+	
+	/* Format */
+	DataStruct->Gyroscope_X = (int16_t)(data[0] << 8 | data[1]);
+	DataStruct->Gyroscope_Y = (int16_t)(data[2] << 8 | data[3]);
+	DataStruct->Gyroscope_Z = (int16_t)(data[4] << 8 | data[5]);
+
+	/* Return OK */
+	return TM_MPU6050_Result_Ok;
+}
+
+TM_MPU6050_Result_t MPU6050_ReadTemperature(TM_MPU6050_t* DataStruct) {
+	uint8_t data[2];
+	int16_t temp;
+	
+	/* Read temperature */
+	I2C_ReadMulti(MPU6050_I2C, DataStruct->Address, MPU6050_TEMP_OUT_H, data, 2);
+	
+	/* Format temperature */
+	temp = (data[0] << 8 | data[1]);
+	DataStruct->Temperature = (float)((int16_t)temp / (float)340.0 + (float)36.53);
+	
+	/* Return OK */
+	return TM_MPU6050_Result_Ok;
+}
+
+TM_MPU6050_Result_t MPU6050_ReadAll(TM_MPU6050_t* DataStruct) {
+	uint8_t data[14];
+	int16_t temp;
+	
+	/* Read full raw data, 14bytes */
+	I2C_ReadMulti(MPU6050_I2C, DataStruct->Address, MPU6050_ACCEL_XOUT_H, data, 14);
+	
+	/* Format accelerometer data */
+	DataStruct->Accelerometer_X = (int16_t)(data[0] << 8 | data[1]);	
+	DataStruct->Accelerometer_Y = (int16_t)(data[2] << 8 | data[3]);
+	DataStruct->Accelerometer_Z = (int16_t)(data[4] << 8 | data[5]);
+
+	/* Format temperature */
+	temp = (data[6] << 8 | data[7]);
+	DataStruct->Temperature = (float)((float)((int16_t)temp) / (float)340.0 + (float)36.53);
+	
+	/* Format gyroscope data */
+	DataStruct->Gyroscope_X = (int16_t)(data[8] << 8 | data[9]);
+	DataStruct->Gyroscope_Y = (int16_t)(data[10] << 8 | data[11]);
+	DataStruct->Gyroscope_Z = (int16_t)(data[12] << 8 | data[13]);
+
+	/* Return OK */
+	return TM_MPU6050_Result_Ok;
+}
+
+uint8_t MPU6050_I2C_IsDeviceConnected(I2C_TypeDef* I2Cx, uint8_t address) {
+	uint8_t connected = 0;
+	/* Try to start, function will return 0 in case device will send ACK */
+	if (!I2C_Start(I2Cx, address, I2C_Direction_Transmitter, I2C_Ack_Enable)) {
+		connected = 1;
+	}
+
+	/* STOP I2C */
+	I2C_Stop(I2Cx);
+
+	/* Return status */
+	return connected;
+}
+
+int16_t I2C_Start(I2C_TypeDef* I2Cx, uint8_t address, uint8_t direction, uint16_t ack) {
+	/* Generate I2C start pulse */
+	I2Cx->CR1 |= I2C_CR1_START;
+
+	/* Wait till I2C is busy */
+	MPU6050_I2C_Timeout = MPU6050_I2C_TIMEOUT;
+	while (!(I2Cx->SR1 & I2C_SR1_SB)) {
+		if (--MPU6050_I2C_Timeout == 0x00) {
+			return 1;
+		}
+	}
+
+	/* Enable ack if we select it */
+	if (ack) {
+		I2Cx->CR1 |= I2C_CR1_ACK;
+	}
+
+	/* Send write/read bit */
+	if (direction == I2C_Direction_Transmitter) {
+		/* Send address with zero last bit */
+		I2Cx->DR = address & ~I2C_OAR1_ADD0;
+
+		/* Wait till finished */
+		MPU6050_I2C_Timeout = MPU6050_I2C_TIMEOUT;
+		while (!(I2Cx->SR1 & I2C_SR1_ADDR)) {
+			if (--MPU6050_I2C_Timeout == 0x00) {
+				return 1;
+			}
+		}
+	}
+	if (direction == I2C_Direction_Receiver) {
+		/* Send address with 1 last bit */
+		I2Cx->DR = address | I2C_OAR1_ADD0;
+
+		/* Wait till finished */
+		MPU6050_I2C_Timeout = MPU6050_I2C_TIMEOUT;
+		while (!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED)) {
+			if (--MPU6050_I2C_Timeout == 0x00) {
+				return 1;
+			}
+		}
+	}
+
+	/* Read status register to clear ADDR flag */
+	I2Cx->SR2;
+
+	/* Return 0, everything ok */
+	return 0;
+}
+
+uint8_t I2C_Stop(I2C_TypeDef* I2Cx) {
+	/* Wait till transmitter not empty */
+	MPU6050_I2C_Timeout = MPU6050_I2C_TIMEOUT;
+	while (((!(I2Cx->SR1 & I2C_SR1_TXE)) || (!(I2Cx->SR1 & I2C_SR1_BTF)))) {
+		if (--MPU6050_I2C_Timeout == 0x00) {
+			return 1;
+		}
+	}
+
+	/* Generate stop */
+	I2Cx->CR1 |= I2C_CR1_STOP;
+
+	/* Return 0, everything ok */
+	return 0;
+}
+
+uint8_t I2C_Read(I2C_TypeDef* I2Cx, uint8_t address, uint8_t reg) {
+	uint8_t received_data;
+	I2C_Start(I2Cx, address, I2C_Direction_Transmitter, I2C_Ack_Disable);
+	I2C_WriteData(I2Cx, reg);
+	I2C_Stop(I2Cx);
+	I2C_Start(I2Cx, address, I2C_Direction_Receiver, I2C_Ack_Disable);
+	received_data = I2C_ReadNack(I2Cx);
+	return received_data;
+}
+
+uint8_t I2C_ReadAck(I2C_TypeDef* I2Cx) {
+	uint8_t data;
+
+	/* Enable ACK */
+	I2Cx->CR1 |= I2C_CR1_ACK;
+
+	/* Wait till not received */
+	MPU6050_I2C_Timeout = MPU6050_I2C_TIMEOUT;
+	while (!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_RECEIVED)) {
+		if (--MPU6050_I2C_Timeout == 0x00) {
+			return 1;
+		}
+	}
+
+	/* Read data */
+	data = I2Cx->DR;
+
+	/* Return data */
+	return data;
+}
+
+uint8_t I2C_ReadNack(I2C_TypeDef* I2Cx) {
+	uint8_t data;
+
+	/* Disable ACK */
+	I2Cx->CR1 &= ~I2C_CR1_ACK;
+
+	/* Generate stop */
+	I2Cx->CR1 |= I2C_CR1_STOP;
+
+	/* Wait till received */
+	MPU6050_I2C_Timeout = MPU6050_I2C_TIMEOUT;
+	while (!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_RECEIVED)) {
+		if (--MPU6050_I2C_Timeout == 0x00) {
+			return 1;
+		}
+	}
+
+	/* Read data */
+	data = I2Cx->DR;
+
+	/* Return data */
+	return data;
+}
+
+void I2C_ReadMulti(I2C_TypeDef* I2Cx, uint8_t address, uint8_t reg, uint8_t* data, uint16_t count) {
+	uint8_t i;
+	I2C_Start(I2Cx, address, I2C_Direction_Transmitter, I2C_Ack_Enable);
+	I2C_WriteData(I2Cx, reg);
+	I2C_Stop(I2Cx);
+	I2C_Start(I2Cx, address, I2C_Direction_Receiver, I2C_Ack_Enable);
+	for (i = 0; i < count; i++) {
+		if (i == (count - 1)) {
+			/* Last byte */
+			data[i] = I2C_ReadNack(I2Cx);
+		} else {
+			data[i] = I2C_ReadAck(I2Cx);
+		}
+	}
+}
+
+void I2C_Write(I2C_TypeDef* I2Cx, uint8_t address, uint8_t reg, uint8_t data) {
+	I2C_Start(I2Cx, address, I2C_Direction_Transmitter, I2C_Ack_Disable);
+	I2C_WriteData(I2Cx, reg);
+	I2C_WriteData(I2Cx, data);
+	I2C_Stop(I2Cx);
+}
+
+void I2C_WriteData(I2C_TypeDef* I2Cx, uint8_t data) {
+	/* Wait till I2C is not busy anymore */
+	MPU6050_I2C_Timeout = MPU6050_I2C_TIMEOUT;
+	while (!(I2Cx->SR1 & I2C_SR1_TXE) && MPU6050_I2C_Timeout) {
+		MPU6050_I2C_Timeout--;
+	}
+
+	/* Send I2C data */
+	I2Cx->DR = data;
+}
